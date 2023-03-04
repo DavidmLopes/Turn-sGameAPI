cd64606aa6d86b3ff5a0d9fa237d4f047f479a42
David Lopes <david.mlopes15@gmail.com>
2023-03-04 14:05:23 +0000

    Generated Spring Project

---
+++ code/jvm/turns-game-api/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "turns-game-api"
---
+++ code/jvm/turns-game-api/src/main/kotlin/pt/isel/turnsgameapi/TurnsGameApiApplication.kt
@@ -0,0 +1,11 @@
+package pt.isel.turnsgameapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TurnsGameApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<TurnsGameApiApplication>(*args)
+}
