82f5169d74dcaa03c08bc501e2e67ed27a3ceb09
David Lopes <david.mlopes15@gmail.com>
2023-03-04 16:18:28 +0000

    Generated Spring Project
    With Spring Web dependencies.

--- code/jvm/turns-game-api/settings.gradle.kts
+++
@@ -1,1 +0,0 @@
-rootProject.name = "turns-game-api"
--- code/jvm/turnsgameapi/src/main/kotlin/pt/isel/turnsgameapi/TurnsgameapiApplication.kt
+++ code/jvm/turnsgameapi/src/main/kotlin/pt/isel/turnsgameapi/TurnsgameapiApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class TurnsGameApiApplication
+class TurnsgameapiApplication
 
 fun main(args: Array<String>) {
-	runApplication<TurnsGameApiApplication>(*args)
+	runApplication<TurnsgameapiApplication>(*args)
 }
